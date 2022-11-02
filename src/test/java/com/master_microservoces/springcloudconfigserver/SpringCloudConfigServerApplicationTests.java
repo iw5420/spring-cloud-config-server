b471425dbdfd7524310d13ada4e5343f2a602215
Ian <iw5420@gmail.com>
2022-11-02 11:16:31 +0800

    first commit

---
+++ src/test/java/com/master_microservoces/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.master_microservoces.springcloudconfigserver;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
